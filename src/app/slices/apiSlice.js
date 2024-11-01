e7b40d177396d62f3fb5fd209ac1e1ace9707a78
kirengamartial <kirengamartial@gmail.com>
2024-11-01 20:43:46 +0200

    env variables

--- src/app/slices/apiSlice.js
+++ src/app/slices/apiSlice.js
@@ -2,7 +2,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/api`,
     credentials: "include",
 })
 
